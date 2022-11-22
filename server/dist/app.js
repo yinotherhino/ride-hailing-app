c621bdc88818c8b632819aacc2a7f7f05403a70c
adigun samuel <adigunsamuel@adiguns-MacBook-Air.local>
2022-11-22 23:09:59 +0100

    typescript configuration

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,25 +1,28 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var app = express();
+const http_errors_1 = __importDefault(require("http-errors"));
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+const index_1 = __importDefault(require("./routes/index"));
+const users_1 = __importDefault(require("./routes/users"));
+const app = (0, express_1.default)();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path_1.default.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use((0, morgan_1.default)('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use('/', index_1.default);
+app.use('/users', users_1.default);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+    next((0, http_errors_1.default)(404));
 });
 module.exports = app;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,34 +1,30 @@
-import { NextFunction } from "express";
+import createError from 'http-errors'
+import express, { Request, Response, NextFunction } from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import indexRouter from './routes/index'
+import usersRouter from './routes/users'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req:Request, res:Response, next:NextFunction) {
-  next(createError(404));
-});
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  next(createError(404))
+})
 
-
-
-module.exports = app;
+module.exports = app
