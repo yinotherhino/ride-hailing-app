4809e4cf1823c0222ee069a47f4b46c02682a76b
Erhimuebru <erhimu.ebru@decagon.dev>
2022-11-22 19:32:31 +0100

    team-4

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,11 @@
-import { NextFunction } from "express";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req:Request, res:Response, next:NextFunction) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,11 +1,12 @@
-import { NextFunction } from "express";
-import { Request, Response } from "express";
-var express = require('express');
-var router = express.Router();
+import { Request, Response, NextFunction } from "express";
+
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req:Request, res:Response, next:NextFunction) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+
+export default router;
