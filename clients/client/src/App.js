f5346ed57beddbed31017ba45a5d57178ba6d177
adigun samuel <adigunsamuel@adiguns-MacBook-Air.local>
2022-11-24 23:58:05 +0100

    merge with yino branch

--- clients/client/src/App.js
+++ clients/client/src/App.js
@@ -1,26 +1,19 @@
-import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
-import './App.css';
-import Test from "./maptest/Test";
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import './App.css'
+import Test from './maptest/Test'
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Link to="/map"> Map </Link>
+      <div className='App'>
+        <Link to='/map'> Map </Link>
         <Routes>
-          <Route exact path='/map' element={< Test />}></Route>
+          <Route exact path='/map' element={<Test />}></Route>
         </Routes>
       </div>
     </Router>
-  );
+  )
 }
 
-
-
-export default App;
+export default App
--- clients/server/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import {Sequelize} from "sequelize"
-import dotenv from 'dotenv'
-dotenv.config()
-
-export const db = new Sequelize('app', '', '',{
-    storage:"./carhailing.sqlite",
-    dialect: "sqlite",
-    logging: false,
-})
